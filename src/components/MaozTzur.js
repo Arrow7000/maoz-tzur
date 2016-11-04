93316c2ae5e2d87ef684e9adb6076e63c80901fe
Aron Adler <aronadler@hotmail.com>
2016-11-04 12:52:58 +0000

    Toggle working & design virtually complete.

--- src/components/MaozTzur.js
+++ src/components/MaozTzur.js
@@ -8,46 +8,47 @@ import Footer from './Footer';
 import getTimes from './getTimes';
 
 class MaozTzur extends Component {
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		getTimes()
-			.then(res => console.log(res));
+        getTimes()
+            .then(res => console.log(res));
 
-		this.state = {
-			languages: content.languages,
-			nusachot: content.versions,
-			selected: {
-				language: 'english',
-				nusach: 'ashkenaz'
-			}
-		};
-		this.selectHandler = this.selectHandler.bind(this);
-	}
-	selectHandler(type, selection) {
-		const newSel = Object.assign({}, this.state.selected, { [type]: selection })
-		this.setState({ selected: newSel });
-	}
-	render() {
-		const { language, nusach } = this.state.selected;
+        this.state = {
+            languages: content.languages,
+            nusachot: content.versions,
+            selected: {
+                language: 'english',
+                nusach: 'ashkenaz'
+            }
+        };
+        this.selectHandler = this.selectHandler.bind(this);
+    }
+    selectHandler(type, selection) {
+        const newSel = Object.assign({}, this.state.selected, { [type]: selection })
+        this.setState({ selected: newSel });
+    }
+    render() {
 
-		return (
-			<section className="MaozTzur">
-				<Selector
-					selected={this.state.selected}
-					languages={this.state.languages}
-					nusachot={this.state.nusachot}
-					handler={this.selectHandler} />
-				<div className="MaozTzur__inner">
-					<Menorah />
-					{content[language][nusach].map(section => <Section
-						section={section}
-						key={section.title}
-						language={this.state.selected.language} />)}
-					<Footer />
-				</div>
-			</section>)
-	}
+        const { language, nusach } = this.state.selected;
+
+        return (
+            <section className="MaozTzur">
+                <Selector
+                    selected={this.state.selected}
+                    languages={this.state.languages}
+                    nusachot={this.state.nusachot}
+                    handler={this.selectHandler} />
+                <div className="MaozTzur__inner">
+                    <Menorah />
+                    {content[language][nusach].map(section => <Section
+                        section={section}
+                        key={section.title}
+                        language={this.state.selected.language} />)}
+                    <Footer />
+                </div>
+            </section>)
+    }
 }
 
 export default MaozTzur;
