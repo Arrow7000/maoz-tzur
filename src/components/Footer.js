e8552de1ef8dc7a65eca25dabc48d01b4f401780
Aron Adler <aronadler@hotmail.com>
2018-11-15 13:36:34 +0000

    Update email address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
 	return (
 		<section className="Footer" id="footer">
-			<p className="credits">Website by <a href="mailto:aron@aawebdesign.co" target="_blank">Aron Adler</a></p>
+			<p className="credits">Website by <a href="mailto:aron@smartowldesign.com" target="_blank">Aron Adler</a></p>
 		</section>
 	)
 }
